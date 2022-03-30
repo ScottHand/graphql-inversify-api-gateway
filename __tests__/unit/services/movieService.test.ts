f37a4a8c01d6bdab442eb92b8c5bd9d20d29afb3
Scott Hand <willhand@amazon.com>
2022-03-30 06:50:32 -0400

    movieservice tests: removed duplicate code and added error test for getById

--- __tests__/unit/services/movieService.test.ts
+++ __tests__/unit/services/movieService.test.ts
@@ -2,69 +2,50 @@ import { Logger } from '../../../src/lib/logger';
 import { MovieService } from '../../../src/services';
 import typeorm = require('typeorm')
 import { Movie } from '../../../src/entities/movie';
+import { InternalServerError } from '../../../src/lib/errors/internalServerError';
 
-jest.mock('../../../src/lib/logger');
-const loggerMock = new Logger();
+let expectedValue;
+let mockLogger;
+let movieService;
 
 describe.only('MovieService', () => {
+  beforeAll(() => {
+    setup();
+  })
   describe('getById', () => {
-    it('should return a single movie', async () => {
-      const expectedValue = {
-        id: 1,
-        title: 'title',
-        description: 'description',
-        releaseDate: '2020-10-01',
-        createdBy: 'somebody',
-        createdDate: '2020-09-01',
-        updatedBy: 'somebodyelse',
-        updatedDate: '2020-09-02',
-      };
-
+    beforeAll(() => {
       const mockQuerybuilder = jest.fn().mockReturnValue({
         select: jest.fn().mockReturnThis(),
         where: jest.fn().mockReturnThis(),
-        getOne: jest.fn().mockResolvedValue( expectedValue )
+        getOne: jest.fn().mockResolvedValue( expectedValue[0] )
       });
 
       typeorm.getConnection = jest.fn().mockReturnValue({
         getRepository: jest.fn().mockReturnValue({createQueryBuilder: mockQuerybuilder})
       });
-
-      const movieService = new MovieService(loggerMock);
-      const result = await movieService.getById(1)
-
-      expect(result).toEqual(expectedValue)
+    });
+    it('should return a single movie when valid request', async () => {
+      const result = await movieService.getById(1);
+      expect(result).toEqual(expectedValue[0]);
 
       const queryBuilder = typeorm.getConnection().getRepository(Movie).createQueryBuilder;
       expect(queryBuilder).toHaveBeenNthCalledWith(1, 'movie');
       expect(queryBuilder().where).toHaveBeenNthCalledWith(1, 'id = :id', { id: 1 });
       expect(queryBuilder().getOne).toHaveBeenNthCalledWith(1);
     });
+    it('should return an InternalServerError when an error occurs', async () => {
+      try {
+        await movieService.getById(1);
+      } catch ( error ) {
+        expect(error).toBeInstanceOf(InternalServerError);
+        expect(mockLogger.error).toHaveBeenCalledWith(movieService.name,
+          movieService.getById.name,
+          new InternalServerError(error));
+      }
+    });
   });
   describe('getAll', () => {
-    it('should return list of movies', async () => {
-      const expectedValue = [
-        {
-          id: 1,
-          title: 'title',
-          description: 'description',
-          releaseDate: '2020-10-01',
-          createdBy: 'somebody',
-          createdDate: '2020-09-01',
-          updatedBy: 'somebodyelse',
-          updatedDate: '2020-09-02',
-        },
-        {
-          id: 2,
-          title: 'title2',
-          description: 'description2',
-          releaseDate: '2020-10-02',
-          createdBy: 'somebody2',
-          createdDate: '2020-09-02',
-          updatedBy: 'somebodyelse2',
-          updatedDate: '2020-09-03',
-        }];
-
+    it('should return list of movies when valid request', async () => {
       const mockQuerybuilder = jest.fn().mockReturnValue({
         orderBy: jest.fn().mockReturnThis(),
         getMany: jest.fn().mockResolvedValue(expectedValue)
@@ -74,7 +55,6 @@ describe.only('MovieService', () => {
         getRepository: jest.fn().mockReturnValue({createQueryBuilder: mockQuerybuilder})
       });
 
-      const movieService = new MovieService(loggerMock);
       const result = await movieService.getAll();
       expect(result).toEqual(expectedValue);
 
@@ -85,3 +65,31 @@ describe.only('MovieService', () => {
     });
   });
 });
+
+function setup() {
+  jest.mock('../../../src/lib/logger');
+  mockLogger = new Logger();
+  movieService = new MovieService(mockLogger);
+
+  expectedValue = [
+    {
+      id: 1,
+      title: 'title',
+      description: 'description',
+      releaseDate: '2020-10-01',
+      createdBy: 'somebody',
+      createdDate: '2020-09-01',
+      updatedBy: 'somebodyelse',
+      updatedDate: '2020-09-02',
+    },
+    {
+      id: 2,
+      title: 'title2',
+      description: 'description2',
+      releaseDate: '2020-10-02',
+      createdBy: 'somebody2',
+      createdDate: '2020-09-02',
+      updatedBy: 'somebodyelse2',
+      updatedDate: '2020-09-03',
+    }];
+}
